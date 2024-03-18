7cec3cef62f38fd1f42ad135635cf645326f3f4a
eduardomaldonadofernandez21 <edumaldonadofdez21@gmail.com>
2024-03-18 09:52:36 +0000

    changes with tailwind

--- src/components/header/headerIndex.jsx
+++ src/components/header/headerIndex.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 export default function Header() {
     return (
         <div>
-            <nav className="flex items-center justify-between h-20 max-w-6x1 mx-auto">
+            <nav className="flex items-center justify-between h-20 max-w-6xl mx-auto">
                 <Link to={'/'}>
                     <div className="ml-5">
                     <h1 className="text-red-900 font-bold text-xl sm:text-2xl md:text-3xl cursor-pointer tracking-wide">
-                        REACT REDYX SHOPPING CART
+                        REACT REDUX SHOPPING CART
                     </h1>
                     </div>
                 </Link>
-                <ul className="flex items-center space-x-6 text-gray-800 font-semibold">
+                <ul className="flex list-none items-center space-x-6 text-gray-800 font-semibold">
                     <Link to={'/'}>
                         <li className="cursor-pointer">Home</li>
                     </Link>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Circles } from "react-loader-spinner";
+import ProductTile from "../components/product-tile/productTileIndex";
 
 
 export default function Home() {
@@ -32,8 +33,14 @@ export default function Home() {
                         visible={true}
                     />
                 </div>
-            ) : ( null
-
+            ) : ( 
+                <div className="min-h-[80vh] grid sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-4 max-w-6xl mx-auto p-3">
+                    {products && products.length
+                        ? products.map((productItem) => (
+                            <ProductTile product={productItem}/>
+                        ))
+                        : null}
+                </div>
             )}
         </div>
     );
